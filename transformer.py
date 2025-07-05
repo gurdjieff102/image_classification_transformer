0b416f4cde5e58cd0cd75b326961e85b6c6da05b
gurdjieff102 <85542149+gurdjieff102@users.noreply.github.com>
2025-07-05 14:11:32 -0700

    Update transformer.py

--- transformer.py
+++ transformer.py
@@ -102,28 +102,107 @@ class EncoderLayer(nn.Module):
         x = self.norm2(x + self.dropout(ff_out))
         return x
     
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, padding=1, downsample=None):
+        super(ConvBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=padding, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        out = self.relu(x)
+        # out = self.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        if self.downsample is not None:
+            x = self.downsample(x)
+        out = out + x 
+        out = self.relu(x + out)
+        return out 
+    
+class ResNet18(nn.Module):
+    def __init__(self, in_channels, out_channels=64, stride=2, padding=3, n_class=10):
+        super(ResNet18, self).__init__()
+        self.out_channels = out_channels
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=7, stride=stride, padding=padding, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self.__make_layer(64, 2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(64, n_class)
+
+    def __make_layer(self, out_channels, blocks, stride=1):
+        layers = []
+        for _ in range(blocks):
+           layers.append(ConvBlock(self.out_channels, out_channels))
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.avgpool(x)
+        x = x.view(x.shape[0], -1)
+        x = self.fc(x)
+        return x
+
+class LinearNet(nn.Module):
+    def __init__(self, in_dim, dim, dropout=0.1):
+        super(LinearNet, self).__init__()
+        self.linear = nn.Linear(in_dim, dim)
+        layers = []
+        layers.append(nn.Linear(dim, dim))
+        # layers.append(nn.LayerNorm(dim))
+        layers.append(RMSNorm(dim=dim))
+        layers.append(nn.GELU())
+        layers.append(nn.Dropout(dropout))
+
+        self.seq = nn.Sequential(*layers)
+    def forward(self, x):
+        x = self.linear(x)
+        x = self.seq(x)
+        return x
+    
 class TransformerEncoder(L.LightningModule):
     def __init__(self, dim, hidden, n_head, n_layers, n_class, dropout=0.1):
         super(TransformerEncoder, self).__init__()
         self.layers = nn.ModuleList(
             [EncoderLayer(dim, hidden, n_head, dropout) for i in range(n_layers)]
         )
-        self.emb = nn.Linear(28 * 28, dim)
+        self.emb = LinearNet(28*28, dim)
         self.fc = nn.Linear(dim, n_class)
         self.loss_fn = nn.CrossEntropyLoss()
         self.transform = transforms.Compose([transforms.ToTensor()])
-        self.batch_size = 32
+        self.batch_size = 256
 
         self.patch_emb = PatchEmbed(4, 1, 128)
+        self.conv_emb = ResNet18(1)
     def forward(self, x):
-        # b, c, w, h = x.shape
-        # x = x.view(b, ｃ, w *ｈ)
-        # x = self.emb(x)
-        x = self.patch_emb(x)
-        for layer in self.layers:
-            x = layer(x)
+        # linear 
+        b, c, w, h = x.shape
+        x = x.view(b, ｃ, w *ｈ)
+        x = self.emb(x)
         x = x.mean(1)
-        x = self.fc(x)
+
+        # transformer 
+        # x = self.patch_emb(x)
+        # for layer in self.layers:
+        #     x = layer(x)
+        # x = x.mean(1)
+        # x = self.fc(x)
+
+        # conv
+        # x = self.conv_emb(x)
+
         return x
 
     def training_step(self, batch, idx):
@@ -145,7 +224,7 @@ class TransformerEncoder(L.LightningModule):
         return loss
     
     def configure_optimizers(self):
-        return optim.Adam(self.parameters(), lr=0.0005)
+        return optim.Adam(self.parameters(), lr=0.001)
     
     def train_dataloader(self):
         train_ds = datasets.MNIST(root='./data',
@@ -179,6 +258,7 @@ model = TransformerEncoder(dim, dim * 2, n_head, n_layers, n_class, dropout)
 
 trainer = L.Trainer(
     max_epochs=4,
+    accumulate_grad_batches=4,
     accelerator='gpu' if torch.cuda.is_available else 'cpu',
     devices='auto'
 )
